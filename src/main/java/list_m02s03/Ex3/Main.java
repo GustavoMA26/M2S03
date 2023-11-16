ee6b912818c96de39b9803f7c7d0cd5980d88822
Gustavo <gmalmeida26@gmail.com>
2023-11-15 21:28:37 -0300

    Ex 3 - POO: Static

---
+++ src/main/java/list_m02s03/Ex3/Main.java
@@ -0,0 +1,10 @@
+package list_m02s03.Ex3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String frase = "Um dois tres quatro";
+        int qtd = Contador.contarPalavras(frase);
+        System.out.println(qtd);
+    }
+}
